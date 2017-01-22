d4457e8fc67dadcf6dd591b927f9b3769a7b01d2
George Higbie <gehigbie@gmail.com>
2017-01-22 16:56:29 -0700

    added reset function to reset button

--- jsRGBColorGame.js
+++ jsRGBColorGame.js
@@ -1,6 +1,4 @@
-//this file was created in the command line
-//alert("JS file Connected!");
-var isHard;
+
 var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
@@ -33,6 +31,7 @@ function reset(){
 	//change color of squares
 	for(var i = 0; i < squares.length; i++){
 		if(colors[i]){
+			squares[i].style.display = "block";
 			squares[i].style.background = colors[i];
 		}else{
 			squares[i].style.display = "none";
@@ -75,19 +74,19 @@ function reset(){
 // });
 
 resetButton.addEventListener("click", function(){
-	//generate all new colors
-	colors = generateRandomColors(numSquares);
-	//pick a new random color from array
-	pickedColor = pickColor();
-	colorDisplay.textContent = pickedColor;
-	//change color of squares
-	for(var i = 0; i < squares.length; i++){
-		squares[i].style.background = colors[i];
-	}
-	h1.style.background = "steelblue";
-	messageDisplay.textContent = "";
-	this.textContent = "New Colors";
-
+	reset();
+	// //generate all new colors
+	// colors = generateRandomColors(numSquares);
+	// //pick a new random color from array
+	// pickedColor = pickColor();
+	// colorDisplay.textContent = pickedColor;
+	// //change color of squares
+	// for(var i = 0; i < squares.length; i++){
+	// 	squares[i].style.background = colors[i];
+	// }
+	// h1.style.background = "steelblue";
+	// messageDisplay.textContent = "";
+	// this.textContent = "New Colors";
 });
 
 colorDisplay.textContent = pickedColor;
